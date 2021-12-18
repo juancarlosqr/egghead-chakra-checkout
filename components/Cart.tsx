e14e88b37278eb0f61cbe0c248cba3f730a2f096
Juan Carlos Quintero <juancarlosqr@users.noreply.github.com>
2021-12-18 19:52:36 +0100

    add cart layout

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,4 +1,14 @@
-import { VStack } from '@chakra-ui/react'
+import Image from 'next/image'
+import {
+  Box,
+  Flex,
+  GridItem,
+  Heading,
+  HStack,
+  SimpleGrid,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
 
 const Cart = () => {
   return (
@@ -10,7 +20,67 @@ const Cart = () => {
       alignItems="flex-start"
       bg="gray.50"
     >
-      <h1>Cart 🛒</h1>
+      <VStack spacing={3} alignItems="flex-start">
+        <Heading size="2xl">Your cart</Heading>
+        <Text>If price is too hard on your eyes, try changing the theme.</Text>
+      </VStack>
+      <SimpleGrid columns={2} rowGap={4} width="full">
+        <GridItem colSpan={2}>
+          <Flex alignItems="center" justifyContent="space-between">
+            <HStack alignItems="center" spacing={6}>
+              <Image width={96} height={96} src="/product.png" />
+              <VStack spacing={0}>
+                <Heading size="md">Penny board</Heading>
+                <Text color="gray.600" size="lg">
+                  PNYCOMP27541
+                </Text>
+              </VStack>
+            </HStack>
+            <Heading size="sm" textAlign="right">
+              $119.00
+            </Heading>
+          </Flex>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Text color="gray.600" size="md">
+            Sub-Total
+          </Text>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Heading size="sm" textAlign="right">
+            $119.00
+          </Heading>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Text color="gray.600">Shipping</Text>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Heading size="sm" textAlign="right">
+            $19.99
+          </Heading>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Text color="gray.600">Taxes (estimated)</Text>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Heading size="sm" textAlign="right">
+            $23.80
+          </Heading>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Box as="hr" my={2} />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Text justifySelf="center" color="gray.600">
+            Total
+          </Text>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Heading size="lg" textAlign="right">
+            $162.79
+          </Heading>
+        </GridItem>
+      </SimpleGrid>
     </VStack>
   )
 }
