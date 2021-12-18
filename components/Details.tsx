a1372cd9359298fe3a1ca0077bd5b0b67c3f8a6e
Juan Carlos Quintero <juancarlosqr@users.noreply.github.com>
2021-12-18 18:48:21 +0100

    add heading

--- components/Details.tsx
+++ components/Details.tsx
@@ -1,9 +1,12 @@
-import { VStack } from '@chakra-ui/react'
+import { Heading, Text, VStack } from '@chakra-ui/react'
 
 const Details = () => {
   return (
     <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
-      <h1>Hello Chakra-UI 👋🏼</h1>
+      <VStack spacing={3} alignItems="flex-start">
+        <Heading size="2xl">Your details</Heading>
+        <Text>If you already have an account, click here to log in.</Text>
+      </VStack>
     </VStack>
   )
 }
