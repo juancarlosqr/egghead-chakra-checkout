9fb83629ed5bf00d71d8e39e66207fc15e03ec92
Juan Carlos Quintero <juancarlosqr@users.noreply.github.com>
2021-12-18 19:04:09 +0100

    add details form

--- components/Details.tsx
+++ components/Details.tsx
@@ -1,4 +1,16 @@
-import { Heading, Text, VStack } from '@chakra-ui/react'
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormLabel,
+  GridItem,
+  Heading,
+  Input,
+  Select,
+  SimpleGrid,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
 
 const Details = () => {
   return (
@@ -7,6 +19,50 @@ const Details = () => {
         <Heading size="2xl">Your details</Heading>
         <Text>If you already have an account, click here to log in.</Text>
       </VStack>
+      <SimpleGrid columns={2} columnGap={3} rowGap={6} width="full">
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>First Name</FormLabel>
+            <Input placeholder="Alice" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>Last Name</FormLabel>
+            <Input placeholder="Doe" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <FormControl>
+            <FormLabel>Address</FormLabel>
+            <Input placeholder="Acme Street 21" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>City</FormLabel>
+            <Input placeholder="San Francisco" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>Country</FormLabel>
+            <Select>
+              <option value="de">Germany</option>
+              <option value="uy">Uruguay</option>
+              <option value="ve">Venezuela</option>
+            </Select>
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Checkbox defaultChecked>Ship to the billing address.</Checkbox>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Button size="lg" w="full">
+            Place Order
+          </Button>
+        </GridItem>
+      </SimpleGrid>
     </VStack>
   )
 }
