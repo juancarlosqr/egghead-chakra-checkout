501bd4e01dbde4711791dad9ab480e6c0bea0320
Juan Carlos Quintero <juancarlosqr@users.noreply.github.com>
2021-12-18 18:40:05 +0100

    add layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
 import type { NextPage } from 'next'
+import { Container, Flex, VStack } from '@chakra-ui/react'
 
 const HomePage: NextPage = () => {
   return (
-    <div>
-      <h1>Hello Chakra-UI 👋🏼</h1>
-    </div>
+    <Container maxW="container.xl" p={0}>
+      <Flex h="100vh" py={20}>
+        <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
+          <h1>Hello Chakra-UI 👋🏼</h1>
+        </VStack>
+        <VStack
+          w="full"
+          h="full"
+          p={10}
+          spacing={10}
+          alignItems="flex-start"
+          bg="gray.50"
+        >
+          <h1>Hello Chakra-UI 👋🏼</h1>
+        </VStack>
+      </Flex>
+    </Container>
   )
 }
 
